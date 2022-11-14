831b8ad3a13c576dbe22e583a7d130cd3883d57c
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-11-14 14:47:52 -0300

    views alpha

---
+++ views/menu.py
@@ -0,0 +1,32 @@
+def mainMenu():
+    print('======================================================')
+    print('                 MENU PRINCIPAL                       ')
+    print('======================================================')
+    _mainMenuOptions()
+    print('======================================================')
+
+def _mainMenuOptions():
+    print('1 - GERAR NOVO GRAFO')
+    print('2 - ADICIONAR ARESTA')
+    print('3 - REMOVER ARESTA')
+    print('4 - CHECAR EXISTENCIA DE ARESTA')
+    print('5 - MOSTRAR GRAFO')
+    print('6 - VER QUANTIDADE DE ARESTAS E VERTICES')
+    print('7 - CHECAR GRAU DE UM VERTICE')
+    print('8 - EXECUTAR ALGORITMOS')
+    print('0 - SAIR DO PROGRAMA')
+
+def generateGraphMenu():
+    pass
+def addEdgeMenu():
+    pass
+def removeEdgeMenu():
+    pass
+def checkEdgeExistenceMenu():
+    pass
+def showGraphMenu():
+    pass
+
+
+mainMenu()
+
