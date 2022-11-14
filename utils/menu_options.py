c066e58d52ce7b9d28b376f885143e480ae98c72
waldeci freitas <freitas.wal18@gmail.com>
2022-11-14 20:29:40 -0300

    menu option added

--- utils/menu_options.py
+++ utils/menu_options.py
@@ -1,1 +1,7 @@
-MAIN_MENU_OPTIONS = {f'{option}' for option in range(9)}
+def _generateOptions(quantity):
+    return {f'{option}' for option in range(0,quantity)}
+
+
+MAIN_MENU_OPTIONS = _generateOptions(9)
+
+GENERATE_GRAPH_MENU_OPTIONS = _generateOptions(3)
