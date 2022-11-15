fbd5a044d199aa01873a967d721f88eb8416212f
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-11-15 13:21:48 -0300

    list add edge

--- modules/graph.py
+++ modules/graph.py
@@ -1,11 +1,11 @@
 class Graph:
 
-    def __init__(self, nodesNumber):
-        self.edges = []
+    def __init__(self, nodesNumber) -> None:
         self.nodes = self._generateNodes(nodesNumber)
+        self.edges = []
         self.nodesNumber = len(self.nodes)
 
-    def _generateNodes(self, quantity):
+    def _generateNodes(self, quantity) -> None:
         nodes = []
         for i in range(quantity):
             node = self.Node(i)
@@ -13,29 +13,45 @@ class Graph:
             nodes.append(node)
             
         return nodes
+    
 
-    def hasNode(self, value):
+    def hasNode(self, value) -> bool:
         for node in self.nodes:
             if node.value == value:
                 return True
 
-        return True
+        return False
 
+    def getNumberNodes(self) -> int: len(self.nodes) 
+    
+    def getNumberEdges(self) -> int: len(self.edges)
+
+    # Metodos abstratos
     def addEdge(self, fromNode, toNode): pass
-
     def removedge(self, fromNode, toNode): pass
-
     def hasEdge(self, fromNode, toNode): pass
 
+    def nodeDegree(self, value) -> int: pass
+
+    def printGraph(self) -> None: pass
+
+    def printNodes(self) -> None:
+        print('[', end='') 
+        for node in self.nodes:
+            print(f'{node.value}, ', end='')
+        print('\\0]') 
 
     class Node:
         id = int
-
-        def __init__(self, value):
+        degree = int
+        def __init__(self, value, id=-1, degree=0):
             self.value = value
+            self.id = id
+            self.degree = degree
 
     class Edge:
 
-        def __init__(self, fromNode, toNode):
+        def __init__(self, fromNode, toNode, weight=1):
             self.toNode = toNode
             self.fromNode = fromNode
+            self.weight = weight
--- modules/list_graph.py
+++ modules/list_graph.py
@@ -10,11 +10,13 @@ class ListGraph(Graph):
         super().__init__(numberNodes)
 
     def addEdge(self, fromNode, toNode):
+        if(not self.hasNode(fromNode) or not self.hasNode(toNode)):
+            raise Exception('AMBOS OS VERTICES DEVEM FAZER PARTE DO GRAFO!')
 
         if(self.hasEdge(fromNode, toNode)):
-            raise Exception('NAO EH POSSIVEL ADCIONAR UMA ARESTA EXISTENTE!')
-        else:
-            self.edges.append(self.Edge(fromNode, toNode))
+            raise Exception('NAO EH POSSIVEL ADCIONAR UMA ARESTA JA EXISTENTE!')
+        
+        self.edges.append(self.Edge(fromNode, toNode))
 
     def removeEdge(self, node): pass
     def removeEdge(self, fromNode, toNode): pass
@@ -23,9 +25,16 @@ class ListGraph(Graph):
     def hasEdge(self, fromNode, toNode):
 
         for edge in self.edges:
-            if edge.fromNode == fromNode and edge.toNode:
+            if edge.fromNode == fromNode and edge.toNode == toNode:
                 return True
 
         return False
 
 
+g = ListGraph(5)
+g.addEdge(1,2)
+try:
+  g.addEdge(10,4)
+  
+except Exception as error:
+    print(error)
