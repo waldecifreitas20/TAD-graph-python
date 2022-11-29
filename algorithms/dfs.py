dea998ab880d3e67a97ea80bfd1178189c2ad572
waldeci freitas <freitas.wal18@gmail.com>
2022-11-29 20:02:25 -0300

    edges classification

--- algorithms/dfs.py
+++ algorithms/dfs.py
@@ -1,20 +1,77 @@
 from utils.colors import *
+from utils.checkers import isDirectionedGraph
+
 
 _MAX_VALUE_ = 999999999
+
 
 class DepthFirstSearch:
 
     def __init__(self, graph) -> None:
         self.graph = graph
         self.colors = []
-        self.dicoveryTime = []
+        self.discoveryTime = []
         self.finalTime = []
-        self.runtime = 0
-        self._initVariables()
+        self.edgeTypes = []
 
     def _initVariables(self):
+        self.runtime = 0
+        self.colors.clear()
+        self.finalTime.clear()
+        self.discoveryTime.clear()
+        self.edgeTypes.clear()
+
         for _ in range(self.graph.getNumberNodes()):
             self.colors.append(WHITE)
-            self.dicoveryTime.append(_MAX_VALUE_)
             self.finalTime.append(_MAX_VALUE_)
-    
+            self.discoveryTime.append(_MAX_VALUE_)
+
+    def getEdgesTypes(self):
+        self._initVariables()
+
+        for node in range(self.graph.getNumberNodes()):
+            if self.colors[node] == WHITE:
+                self._classifyEdgesDFS(node)
+        return self.edgeTypes
+
+
+    def _classifyEdgesDFS(self, node):
+        self.runtime += 1
+        self.colors[node] = GRAY
+        self.discoveryTime[node] = self.runtime
+
+        adjacents = self.graph.getAdjacentsFrom(node)
+        adjacents.sort(reverse=True)
+
+        if len(adjacents) != 0:
+            while True:
+                try:
+                    destinyNode = adjacents.pop()
+                    if self.colors[destinyNode] == WHITE:
+                        self.edgeTypes.append([node, destinyNode, self.EdgeType.TREE])
+                        self._classifyEdgesDFS(destinyNode)
+                    else:
+                        isBackEdge = not isDirectionedGraph(self.graph) or self.colors[destinyNode] == GRAY
+                        if isBackEdge:
+                            self.edgeTypes.append([node, destinyNode, self.EdgeType.BACK])
+                            
+                        elif self.colors[destinyNode] == BLACK:
+                            
+                                isForwardEdge = self.discoveryTime[destinyNode] > self.discoveryTime[node] 
+                                if isForwardEdge:
+                                    self.edgeTypes.append([node, destinyNode, self.EdgeType.FORWARD])
+                                else:
+                                    self.edgeTypes.append([node, destinyNode, self.EdgeType.CROSS])
+                except:
+                    break
+
+        self.runtime += 1
+        self.finalTime[node] = self.runtime
+        self.colors[node] = BLACK
+
+
+    class EdgeType:
+        TREE = '__TREE__'
+        BACK = '__BACK__'
+        CROSS = '__CROSS__'
+        FORWARD = '__FORWARD__'
--- test.py
+++ test.py
@@ -1,9 +1,38 @@
-from modules.matrix_directioned_graph import *
-from modules.matrix_graph import *
+from modules.list_directioned_graph import *
+from modules.list_graph import *
 from algorithms.dfs import DepthFirstSearch
 
-g = MatrixGraph(5)
+g = ListGraph(5)
 
-dfs = DepthFirstSearch(g)
+g.addEdge(1,1)
 
-print(dfs.colors)
+
+dg = DirectionedListGraph(7)
+
+
+dg.addEdge(1,2)
+dg.addEdge(1,5)
+
+dg.addEdge(2,5)
+dg.addEdge(2,3)
+
+dg.addEdge(3,6)
+
+dg.addEdge(6,2)
+
+dg.addEdge(5,6)
+
+dg.addEdge(4,5)
+dg.addEdge(4,1)
+
+
+
+
+
+dfs = DepthFirstSearch(dg)
+
+
+et = dfs.getEdgesTypes()
+
+
+print(et)
