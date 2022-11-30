38dc027ced32a7116856559059dc875bc7ddd2db
WaldeciFreitas <62445806+waldecifreitas20@users.noreply.github.com>
2022-11-30 16:08:30 -0300

    topological sorting finished

--- test.py
+++ test.py
@@ -7,21 +7,37 @@ g = ListGraph(5)
 g.addEdge(1,1)
 
 
-dg = DirectionedListGraph(3)
+dg = DirectionedListGraph(10)
 
+dg.addEdge(0,1)
+dg.addEdge(0,5)
+dg.addEdge(0,3)
+dg.addEdge(0,2)
 
-dg.addEdge(1,0)
 dg.addEdge(1,2)
 
+dg.addEdge(2,3)
+dg.addEdge(2,4)
+
+dg.addEdge(4,6)
+
+dg.addEdge(5,4)
+dg.addEdge(5,6)
+
+dg.addEdge(6,7)
+dg.addEdge(6,8)
+
+dg.addEdge(7,8)
+
+dg.addEdge(9,6)
 
 
 dfs = DepthFirstSearch(dg)
 
 
-r = dfs.getTopologicalSorting(2)
+r = dfs.getTopologicalSorting(0)
 print(r)
-print(dfs.discoveryTime)
-print(dfs.finalTime)
+
 """ 
 for edge in et:
     print(f'{edge["origin"]}, {edge["destiny"]} = {edge["type"]}') """
